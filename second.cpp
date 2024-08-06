918920cdb30c6becf3dfdcfb835b382c0bc0d600
nivrithy <nikhilnivrithy@gmail.com>
2024-08-06 18:20:11 +0530

    Add files via upload

---
+++ second.cpp
@@ -0,0 +1,43 @@
+// INCREMENT AND DECREMENT OPERATION
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+    //int i=1;
+    //cout << "the value of i:"<<i<<endl;
+    //i=i-1;
+    //cout <<"the decreased value of i:"<<i<<endl;
+    //i-=1;
+    //cout<<"the decreased value of i:"<<i<<endl;
+    //return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+    //int i=1;
+    //cout<<i<<endl;
+    //cout<<++i<<endl;
+    //cout<<i<<endl;
+    //return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+    //cout<<sizeof(int)<<endl;
+    //cout<<sizeof(float)<<endl;
+    //cout<<sizeof(char)<<endl;
+    //cout<<sizeof(double)<<endl;
+    //return 0;
+//}
+
+//OPERATOR PRECEDENCE
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+    //int value=(2*1+1-2*5);
+    //cout <<"value is:"<<value<<endl;
+    //return 0;
+//}
