420abe81aa6ffdfe6f024a4065f55b993fd511e5
nivrithy <nikhilnivrithy@gmail.com>
2024-08-06 17:13:06 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,101 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+    //cout << "hello , c++" << std::endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main() {
+    //string myname="nivrithy\ni'm 17";
+    //cout<<myname<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main() {
+    //string sentence= R"(
+    //hi students
+    //ths is an amazing opportunity
+    //dont miss it!)";
+    //cout << sentence<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main() {
+    //string name="subi";
+    //int age=18;
+
+    //string msg="hi"+name+"! you are "+ std::to_string(age)+
+    //"years old...today is ur bday,happy bday!!\n";
+    //cout<< msg<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include<string>
+//using namespace std;
+//int main() {
+    //string msg="hi ths s mirra";
+    //cout << "size is:" << msg.size()<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main() {
+    //std::string emoji="\U0001F604 \U0001F680 \n";
+    //std::cout<<"hello guys:"<<emoji;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main() {
+    //double Double;
+    //Double=1.23;
+    //cout<<"value of Double is:"<<Double<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main() {
+    //auto name="nivi";
+    //auto age=17;
+    //auto weight=41.5;
+    //cout << "name:"<<name << endl;
+    //cout <<"age:"<<age<<endl;
+    //cout<<"weight:"<<weight <<endl;
+    //return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+    //float age=23.6;
+    //int x;
+    //x=age;
+    //cout<<"age is:"<<x<<" bytes is:"<<sizeof(x)<<endl;
+    //return 0;
+//}
+
+#include <iostream>
+using namespace std;
+int main() {
+    int students,sections;
+    students=1200;sections=12;
+    int total=students%sections;
+    cout<<total<<" students are reamining "<<endl;
+    return 0;
+}
