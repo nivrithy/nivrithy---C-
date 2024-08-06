785209ed014c7b7fec536d9cf3583afbc99fef0d
nivrithy <nikhilnivrithy@gmail.com>
2024-08-06 18:20:25 +0530

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,80 @@
+//#include <iostream>
+//#include<cmath>
+//using namespace std;
+//int main() {
+    //int y=5;
+    //int square=pow(y,2);
+    //cout<<"square of y is:"<<square<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include<cmath>
+//using namespace std;
+//int main() {
+    //double num=25;
+    //double x=sqrt(num);
+    //cout<<"square root:"<<x<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include<cmath>
+//using namespace std;
+//int main() {
+    //double x=100.0;
+    //double naturallog=log(x);
+    //double baselog=log10(x);
+    //cout<<"natural log of"<<x<<"is:"<<naturallog<<endl;
+    //cout<<"base-10 log of"<<x<<"is:"<<baselog<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include<cmath>
+//using namespace std;
+//int main() {
+    //double number=4.56;
+    //double rounnumber=round(number);
+    //cout<<"rounded number is:"<<rounnumber<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include<cmath>
+//using namespace std;
+//int main() {
+    //double num=15.7;
+    //double absvalue=fabs(num);
+    //cout<<"absolute value is:"<< absvalue<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main() {
+    //double pi=M_PI;
+    //double euler=M_E;
+    //double log2e=M_LOG2E;
+    //double log10e=M_LOG10E;
+    //cout<<"pi"<<pi<<endl;
+    //cout<<"euler's number"<<euler<<endl;
+    //cout<<"base-2 log of e"<<log2e<<endl;
+    //cout<<"base-10 log of e"<<log10e<<endl;
+    //return 0;
+//}
+
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main() {
+    //double angle=90.0;
+    //double radianangle=angle*M_PI/180.0;
+    //double sinresult=sin(radianangle);
+    //double cosresult=cos(radianangle);
+    //cout<<"sin("<<angle<<"degrees)="<<sinresult<<endl;
+    //cout<<"cos("<<angle<<"degress)="<<cosresult<<endl;
+    //return 0;
+//}
+
