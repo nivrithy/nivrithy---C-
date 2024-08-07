c67c6aaed008846cfab3b4925a4ffa1db74500d8
nivrithy <nikhilnivrithy@gmail.com>
2024-08-07 18:23:35 +0530

    Add files via upload

---
+++ flow.cpp
@@ -0,0 +1,99 @@
+//COMAPARISON OPERATORS
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+    //int x=21;
+    //int y=21;
+    //if (x==y){
+        //cout<<"both are same"<<endl;
+    //} else {
+        //cout<<"both are not same"<<endl;
+    //}
+    ///return 0;
+//}
+
+//include<iostream>
+//using namespace std ;
+//int main() {
+    //int x=6;
+    //int y=7;
+    //if (x<y) {
+        //cout<<"x is small"<<endl;
+    //} else {
+        //cout<<"y is small"<<endl;
+    //}
+    //return 0;
+//}
+
+//SINGLE CONDITION
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+    //bool terms;
+    //terms=true;
+    //if(terms==true) {
+        //cout<<"accepted"<<endl;
+    //} else {
+        //cout<<"denied"<<endl;
+    //}
+    //return 0;
+//}
+
+//DOUBLE CONDITION
+
+//#include <iostream>
+//#include<list>
+//using namespace std;
+//int main() {
+    //int age;
+    //age=8;
+    //if(age<10){
+        //cout<<"cartoon movies"<<endl;
+    //} else {
+        //cout<<"horror movies"<<endl;
+    //}
+    //return 0;
+//}
+
+// MANY CONDITION
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+    //string state;
+    //state="kerala";
+    //if (state=="tamilnadu") {
+        //cout<<"tamil movies"<<endl;
+    //} else if (state=="karnataka") {
+        //cout<<"kanada movies"<<endl;
+    //}else if (state=="andhra pradesh") {
+        //cout<<"telugu movies"<<endl;
+    //} else {
+        //cout<<"english movies"<<endl;
+    //}
+    //return 0;
+//}
+
+// SWITCH STATEMENT
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+    //char x;
+    //cout<<"enter any upper case letter : "<<endl;
+    //cin>>x;
+    //switch (x+32) {
+        //case 'a':
+        //case 'e':
+        //case 'i':
+        //case 'o':
+        //case 'u':
+            //cout<<"vowels"<<endl;
+            //break;
+        //default:
+             //cout<<"consonants"<<endl;
+    //}
+    //return 0;
+//}
