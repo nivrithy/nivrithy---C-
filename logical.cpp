1e8f6f22249d0d2dde6708cf80cec2efec343669
nivrithy <nikhilnivrithy@gmail.com>
2024-08-08 18:08:52 +0530

    Add files via upload

---
+++ logical.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     string idly="good";
+//     string chutney="good";
+//     if (idly=="good" && chutney=="good") {
+//         cout<<"breakfast was good";
+//     } else {
+//         cout<<"breakfast was bad";
+//     }
+//     cout<<endl<<endl;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     string idly="good";
+//     string chutney="good";
+//     if (idly=="bad" || chutney=="good") {
+//         cout<<"breakfast was good";
+//     } else {
+//         cout<<"breakfast was bad";
+//     }
+//     cout<<endl<<endl;
+//     return 0;
+// }
