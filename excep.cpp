9434b8f53ed948cd0021fad76996489b212a3ec3
nivrithy <nikhilnivrithy@gmail.com>
2024-08-09 18:18:38 +0530

    Add files via upload

---
+++ excep.cpp
@@ -0,0 +1,19 @@
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int num,den;
+//     cout<<"numerator : ";
+//     cin>>num;
+//     cout<<"denominator : ";
+//     cin>>den;
+//     try {
+//         if (den==0) {
+//             throw runtime_error ("division by 0 is invalid");
+//         }
+//         cout<<"result"<<num/den<<endl;
+//     }
+//     catch ( runtime_error & e) {
+//         cerr<<"error: "<<e.what() <<endl;
+//     }
+//     return 0;
+// }
